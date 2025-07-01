2ef5dfd774cca763f0bcce5b6a28cad43bb8f852
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-07-01 11:04:14 +0530

    support for multiple authorized enrollment clients

--- internal/controller/incomingfingerprintcontroller/incomingEnroll.go
+++ internal/controller/incomingfingerprintcontroller/incomingEnroll.go
@@ -3,6 +3,7 @@ package incomingfingerprintcontroller
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/warnakulasuriya-fds-e23/orchestration-service/internal/requestobjects"
@@ -11,7 +12,8 @@ import (
 
 func (controller *IncomingFingerprintController) incomingEnrollHandler(c *gin.Context) {
 	// check whether the client-id is authorized to be performing the enroll procedure
-	authorizedClientId := os.Getenv("FINGERPRINT_AUTHORIZED_ENROLLMENT_CLIENT_ID")
+	authorizedClientIdsString := os.Getenv("FINGERPRINT_AUTHORIZED_ENROLLMENT_CLIENT_IDS")
+	authorizedClientIds := strings.Split(authorizedClientIdsString, ",")
 	var reqObj requestobjects.SubmitForEnrollReqObj
 	err := c.BindJSON(&reqObj)
 	if err != nil {
@@ -19,7 +21,14 @@ func (controller *IncomingFingerprintController) incomingEnrollHandler(c *gin.Co
 		c.IndentedJSON(http.StatusInternalServerError, resObj)
 		return
 	}
-	if reqObj.ClientId != authorizedClientId {
+	isAuthorized := false
+
+	for _, authorizedClientId := range authorizedClientIds {
+		if authorizedClientId == reqObj.ClientId {
+			isAuthorized = true
+		}
+	}
+	if !isAuthorized {
 		resObj := responseobjects.ErrorResObj{Message: "Permission Denied client that submit for enrollment is not authorized to use this feature "}
 		c.IndentedJSON(http.StatusUnauthorized, resObj)
 		return
