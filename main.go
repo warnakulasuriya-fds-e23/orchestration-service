75db0924d5398d52368df0b3b7b02fe14f9552a5
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-07-01 10:34:34 +0530

    added request logging

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"os"
 
@@ -10,6 +12,18 @@ import (
 	"github.com/warnakulasuriya-fds-e23/orchestration-service/internal/controller/outgoingfingerprintcontroller"
 	"github.com/warnakulasuriya-fds-e23/orchestration-service/internal/customstorage"
 )
+
+func RequestLoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var buf bytes.Buffer
+		tee := io.TeeReader(c.Request.Body, &buf)
+		body, _ := io.ReadAll(tee)
+		c.Request.Body = io.NopCloser(&buf)
+		log.Println(string(body))
+		log.Println(c.Request.Header)
+		c.Next()
+	}
+}
 
 func main() {
 	_, err := os.Stat(".env")
@@ -30,6 +44,8 @@ func main() {
 	incomingfingerprintcntrlr := incomingfingerprintcontroller.NewIncomingFingerprintController(outgoingfingerprintcntrlr)
 
 	router := gin.Default()
+
+	router.Use(RequestLoggerMiddleware())
 
 	router.POST("/api/fingerprint/identify", incomingfingerprintcntrlr.IncomingIdentifyHandler)
 	router.POST("/api/fingerprint/match", incomingfingerprintcntrlr.IncomingMatchHandler)
